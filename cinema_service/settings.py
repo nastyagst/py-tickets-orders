ed25b445a6be929efcd63d47bbd9758a5fa3a48e
nastya <hociy2015@gmail.com>
2026-01-22 20:34:26 +0200

    Solution2

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,6 +139,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.PageNumberPagination"
+    ),
     "PAGE_SIZE": 10,
 }
